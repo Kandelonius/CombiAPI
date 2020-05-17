313cd04f09559dabe55a4ee165bd7dcf86d1614f
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-17 00:22:10 -0700

    still getting CORS error saying I need to add a header to give access-control-allow-origin to a site.

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,14 +1,17 @@
 // actions index.js
 import axios from "axios";
 
-// const url = "https://api.ipgeolocationapi.com/geolocate HTTP/1.1";
+const url = "https://ipvigilante.com/";
+const format = "json/";
+const IP = "8.8.8.8";
+const params = '';
 export const FETCH = 'FETCH';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
 export const fetchLocale = () => dispatch => {
     dispatch({ type: FETCH });
-    axios.get(url)
+    axios.get(`${url}${format}${IP}${params}"`)
         .then(res => {
             console.log('res ', res);
             // dispatch({ type: FETCH_SUCCESS, payload: res.data });
